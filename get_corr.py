9f554a887d111916b323b565801e3b5264e9ab6e
RyoWakabayashi <gfdmkm573@gmail.com>
2022-06-23 18:21:04 +0900

    feat: Add Explorer

---
+++ get_corr.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+joined_df = pd.read_csv("tmp/joined.csv")
+
+print("平均")
+print(joined_df.mean())
+print("")
+
+print("標準偏差")
+print(joined_df.std())
+print("")
+
+print("共分散")
+print(joined_df.cov())
+print("")
+
+print("相関係数")
+print(joined_df.corr())
+print("")
